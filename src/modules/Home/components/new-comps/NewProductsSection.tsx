11c7d1728c911e8ad43c682d5da86ab042e5b36a
Kudumart <kudumartcorporation@gmail.com>
2026-06-27 00:19:11 +0500

    Update NewProductsSection.tsx

--- src/modules/Home/components/new-comps/NewProductsSection.tsx
+++ src/modules/Home/components/new-comps/NewProductsSection.tsx
@@ -35,7 +35,7 @@ export default function NewProductSection() {
     <div className="w-full">
       <div className="w-full">
         <div className="bg-[#C1FFA5] flex justify-between p-6 rounded-md mb-10 cursor-pointer">
-          <h2 className="text-lg font-semibold">All Products</h2>
+          <h2 className="text-lg font-semibold">Best Products</h2>
           <Link to={"/see-all"} className="text-black font-semibold">
             See All
           </Link>
